b984fd8cfc4b27b094144d6b7daeee188edffa21
Kutman Amangeldiev <kutmanamangeldiev@Kutmans-MacBook-Pro.local>
2024-10-07 23:50:30 +0800

    initial commit

---
+++ Temp Converter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Temp Converter
+//
+//  Created by Kutman Amangeldiev on 2024/10/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding(.top, 20)
+    }
+}
+
+#Preview {
+    ContentView()
+}
